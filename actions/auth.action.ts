01099f02323bef5eccffeffa89496ebbf4d66e8a
OJPalenzuela <palenzuelaomar@gmail.com>
2024-11-27 10:21:52 -0500

    Add registration functionality and refactor login page; implement validation schemas and enhance user experience

--- actions/auth.action.ts
+++ actions/auth.action.ts
@@ -1,8 +1,9 @@
 'use server';
-import type { loginSchema } from '@/lib/zod';
+import { loginSchema, registerSchema } from '@/lib/zod';
 import type { z } from 'zod';
 import { signIn, signOut } from '@/auth';
 import { AuthError } from 'next-auth';
+import { backend } from '@/config/backend';
 
 export async function loginAction(values: z.infer<typeof loginSchema>) {
   try {
@@ -17,6 +18,41 @@ export async function loginAction(values: z.infer<typeof loginSchema>) {
     if (error instanceof AuthError) {
       return { error: error?.cause?.err?.message };
     }
+    return { error: 'An error occurred: 500' };
+  }
+}
+
+export async function registerAction(values: z.infer<typeof registerSchema>) {
+  try {
+    const { data, success } = registerSchema.safeParse(values);
+
+    if (!success) {
+      return { error: 'Invalid credentials' };
+    }
+
+    const user = await backend.post({
+      where: {
+        email: data.email
+      }
+    });
+
+    if (user) {
+      return { error: 'User already exists' };
+    }
+
+    await signIn('credentials', {
+      email: data.email,
+      password: data.password,
+      redirect: false
+    });
+
+    return { success: true };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      return { error: error?.cause?.err?.message };
+    }
+    console.error(error);
+
     return { error: 'An error occurred: 500' };
   }
 }
--- lib/zod.ts
+++ lib/zod.ts
@@ -4,3 +4,13 @@ export const loginSchema = object({
   email: string({ required_error: 'Correo es requerido' }).min(1, 'Correo es requerido').email('Correo no válido'),
   password: string({ required_error: 'Contraseña requerida' }).min(1, 'Contraseña requerida').min(8, 'La contraseña debe tener al menos 8 caracteres').max(32, 'La contraseña debe tener menos de 32 caracteres')
 });
+
+export const registerSchema = object({
+  name: string({ required_error: 'Nombre es requerido' }).min(1, 'Nombre es requerido').max(32, 'Nombre debe tener menos de 32 caracteres'),
+  email: string({ required_error: 'Correo es requerido' }).min(1, 'Correo es requerido').email('Correo no válido'),
+  password: string({ required_error: 'Contraseña requerida' }).min(1, 'Contraseña requerida').min(8, 'La contraseña debe tener al menos 8 caracteres').max(32, 'La contraseña debe tener menos de 32 caracteres'),
+  password_confirmation: string({ required_error: 'Confirmación de contraseña requerida' })
+    .min(1, 'Confirmación de contraseña requerida')
+    .min(8, 'La confirmación de contraseña debe tener al menos 8 caracteres')
+    .max(32, 'La confirmación de contraseña debe tener menos de 32 caracteres')
+});
